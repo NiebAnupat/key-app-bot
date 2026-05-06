6c3109b701a76fcfeb94d0e2cd560f4bd143ac16
Anupat <anupat.kae@gmail.com>
2026-05-06 14:24:09 +0700

    feat: add select "ไม่มีหน่วยงาน" when create order

--- src/index.ts
+++ src/index.ts
@@ -176,6 +176,11 @@ async function createOrder(page: Page, data: OrderData) {
    await page.fill('input[name="lastName"]', data.lastName);
    await page.waitForTimeout(800);
    await page.fill('input[name="phoneNumber"]', data.tel);
+   await page.waitForTimeout(800);
+   // <span class="MuiFormControl-root" style="display: flex; flex-direction: row;"><label><span class="MuiButtonBase-root MuiIconButton-root jss5337 MuiRadio-root MuiRadio-colorSecondary jss5300 MuiIconButton-colorSecondary" aria-disabled="false"><span class="MuiIconButton-label"><input class="jss5340" type="radio" value="1"><div class="jss5341"><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path></svg><svg class="MuiSvgIcon-root jss5342" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.465 8.465C9.37 7.56 10.62 7 12 7C14.76 7 17 9.24 17 12C17 13.38 16.44 14.63 15.535 15.535C14.63 16.44 13.38 17 12 17C9.24 17 7 14.76 7 12C7 10.62 7.56 9.37 8.465 8.465Z"></path></svg></div></span><span class="MuiTouchRipple-root"></span></span>มีหน่วยงาน</label><label><span class="MuiButtonBase-root MuiIconButton-root jss5337 MuiRadio-root MuiRadio-colorSecondary jss5300 MuiIconButton-colorSecondary" aria-disabled="false"><span class="MuiIconButton-label"><input class="jss5340" type="radio" value="0"><div class="jss5341"><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path></svg><svg class="MuiSvgIcon-root jss5342" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.465 8.465C9.37 7.56 10.62 7 12 7C14.76 7 17 9.24 17 12C17 13.38 16.44 14.63 15.535 15.535C14.63 16.44 13.38 17 12 17C9.24 17 7 14.76 7 12C7 10.62 7.56 9.37 8.465 8.465Z"></path></svg></div></span><span class="MuiTouchRipple-root"></span></span>ไม่มีหน่วยงาน</label></span>
+   // select "ไม่มีหน่วยงาน"
+   await page.click('span.MuiFormControl-root label:nth-child(2)');
+   await page.waitForTimeout(800);
    await page
       .locator('button[type="submit"]', { hasText: "ประกันสุขภาพ" })
       .click();
